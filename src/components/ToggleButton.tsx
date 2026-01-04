5c94fc6b87f6ac99afbea0d08a21b24369b1e589
dev-rome <info.devrome@gmail.com>
2026-01-04 10:27:00 -0500

    add ToggleButton.tsx component to Navbar.tsx for light and dark theme switching

---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,22 @@
+import { useTheme } from "../hooks/useTheme";
+import { FaSun, FaMoon } from "react-icons/fa";
+
+export function ToggleButton() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="flex items-center gap-2 lg:cursor-pointer"
+    >
+      {theme === "light" ? (
+        <FaMoon className="text-base lg:text-lg" />
+      ) : (
+        <FaSun className="text-base lg:text-lg" />
+      )}
+      <span className="text-xs font-semibold lg:text-base">
+        {theme === "light" ? "Dark Mode" : "Light Mode"}
+      </span>
+    </button>
+  );
+}
