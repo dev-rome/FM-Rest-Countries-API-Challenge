68340c607129f361f4c469f13028cf83afba91b5
dev-rome <info.devrome@gmail.com>
2026-01-03 20:41:36 -0500

    rest countries api challenge project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="p-4 text-3xl font-bold text-white">
+        Rest Countries API Challenge
+      </h1>
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "@fontsource/nunito-sans/300.css";
+import "@fontsource/nunito-sans/600.css";
+import "@fontsource/nunito-sans/800.css";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
