797fa99b1ccf3f19d87bdffa78758bd3e19da934
dev-rome <info.devrome@gmail.com>
2026-01-04 17:36:27 -0500

    edit padding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import { ToggleButton } from "./ToggleButton";
 
 export function Navbar() {
   return (
-    <header className="bg-surface shadow-[0_2px_4px_0_rgba(0,0,0,0.0562)]">
-      <div className="mx-auto max-w-7xl px-4 py-8 md:px-10 lg:py-6">
+    <header className="bg-surface px-4 shadow-[0_2px_4px_0_rgba(0,0,0,0.0562)] md:px-10 lg:px-20">
+      <div className="mx-auto max-w-7xl py-8 lg:py-6">
         <nav className="flex items-center justify-between">
           <Link to="/" className="font-extrabold md:text-lg lg:text-2xl">
             Where in the world?
