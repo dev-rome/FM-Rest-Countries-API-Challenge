3108545778e783357929216cb6306a41840d8c78
dev-rome <info.devrome@gmail.com>
2026-01-04 17:39:09 -0500

    add /country/:code route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-// import CountryPage from "./pages/CountryPage";
-// import NotFound from "./pages/NotFound";
+import CountryPage from "./pages/CountryPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* <Route path="/country/:code" element={<CountryPage />} /> */}
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="/country/:code" element={<CountryPage />} />
     </Routes>
   );
 }
