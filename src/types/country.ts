a1ada1679af74946ce00d6d125eca8b0d9a5e1c4
dev-rome <info.devrome@gmail.com>
2026-01-04 18:48:41 -0500

    update types

--- src/types/country.ts
+++ src/types/country.ts
@@ -2,10 +2,16 @@ export interface Country {
   name: {
     common: string;
     official: string;
+    nativeName?: Record<string, { common: string }>;
   };
   population: number;
   region: string;
+  subregion?: string;
   capital?: string[];
+  tld?: string[];
+  currencies?: Record<string, { name: string }>;
+  languages?: Record<string, string>;
+  borders?: string[];
   flags: {
     png: string;
     svg: string;
