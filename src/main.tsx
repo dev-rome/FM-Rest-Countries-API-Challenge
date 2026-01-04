4e52d7c00e91c35cf8e8059fec99ed8119a90a4b
dev-rome <info.devrome@gmail.com>
2026-01-04 10:19:23 -0500

    wrap App.tsx with ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import App from "./App.tsx";
 import "@fontsource/nunito-sans/300.css";
 import "@fontsource/nunito-sans/600.css";
@@ -9,8 +10,10 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 );
